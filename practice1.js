6532c4ec9b56ea93a62494fa6bab041a0a45339d
rickie120243 <rickie120243@gmail.com>
2016-02-05 20:48:07 +0800

    expand call function

--- practice1.js
+++ practice1.js
@@ -1,10 +1,17 @@
 function Price(meat) {
+	var increase = 0;
+
 	this.meat = meat;
 //	console.log(this);
+	if(this.staple == "rice") {
+		increase = 10;
+	}else if(this.staple == "noodles") {
+		increase = 5;
+	}
 	if(meat == "beef") {
-		this.price = "100";
+		this.price = 100+increase;
 	}else if (meat == "pork") {
-		this.price = "80";
+		this.price = 80+increase;
 	}
 }
 
@@ -15,10 +22,14 @@ function Meals(name, staple, meat, soup) {
 	this.soup = soup;
 }
 
-var order1 = new Meals("rickie", "rice", "beef", "miso soup");
-var order2 = new Meals("justin", "noodles", "pork", "pumpkin soup");
-
+var order1 = new Meals("Rickie", "rice", "beef", "miso soup");
+var order2 = new Meals("Justin", "noodles", "pork", "pumpkin soup");
+var order3 = new Meals("Sam", "noodles", "beef", "pumpkin soup");
+var order4 = new Meals("John", "rice", "pork", "pumpkin soup");
 
 console.log(order1);
 console.log(order2);
+console.log(order3);
+console.log(order4);
+
 
