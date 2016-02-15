c2a48aba2681e99b9e6d822337497c18199ea7eb
rickie120243 <rickie120243@gmail.com>
2016-02-15 10:34:07 +0800

    fix style

--- practice1.js
+++ practice1.js
@@ -1,30 +1,31 @@
 function Price(meat) {
 	var increase = 0;
 	this.meat = meat;
-//	console.log(this);
-	if(this.staple == "rice") {
+	if ('rice' === this.staple) {
 		increase = 10;
-	}else if(this.staple == "noodles") {
+	}
+	else if ('noodles' === this.staple) {
 		increase = 5;
 	}
-	if(meat == "beef") {
-		this.price = 100+increase;
-	}else if (meat == "pork") {
-		this.price = 80+increase;
+	if ('beef' === meat) {
+		this.price = 100 + increase;
+	}
+	else if ('pork' === meat) {
+		this.price = 80 + increase;
 	}
 }
 
-function Meals(name, staple, meat, soup) {
+function Order(name, staple, meat, soup) {
 	this.name = name;
 	this.staple = staple;
 	Price.call(this, meat);
 	this.soup = soup;
 }
 
-var order1 = new Meals("Rickie", "rice", "beef", "miso soup");
-var order2 = new Meals("Justin", "noodles", "pork", "pumpkin soup");
-var order3 = new Meals("Sam", "noodles", "beef", "pumpkin soup");
-var order4 = new Meals("John", "rice", "pork", "pumpkin soup");
+var order1 = new Order('Rickie', 'rice', 'beef', 'miso soup');
+var order2 = new Order('Justin', 'noodles', 'pork', 'pumpkin soup');
+var order3 = new Order('Sam', 'noodles', 'beef', 'pumpkin soup');
+var order4 = new Order('John', 'rice', 'pork', 'pumpkin soup');
 
 console.log(order1);
 console.log(order2);
